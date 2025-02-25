60474c64f408329e91578a1d906cbd7a35a04586
matheusapostulo <matheusapostulo10@gmail.com>
2025-02-25 15:25:32 -0300

    feat: ShortenURL core service with test

--- internal/url/infra/service/url_shortener_base62.go
+++ internal/url/infra/service/url_shortener_base62.go
@@ -1,12 +1,37 @@
 package service
 
-func NewURLShortener() *URLShortener {
-	return &URLShortener{}
+import (
+	"strings"
+)
+
+func reverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
-type URLShortener struct {
+func NewURLShortenerBase62() *URLShortenerBase62 {
+	return &URLShortenerBase62{}
 }
 
-func (u *URLShortener) ShortenURL(url string) (string, error) {
-	return "", nil
+type URLShortenerBase62 struct {
+}
+
+func (u *URLShortenerBase62) ShortenURL(id int) (string, error) {
+	chars := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var result strings.Builder
+
+	mod := id % 62
+	remainder := id / 62
+	result.WriteByte(chars[mod])
+
+	for remainder > 0 {
+		mod = remainder % 62
+		remainder = remainder / 62
+		result.WriteByte(chars[mod])
+	}
+
+	return reverseString(result.String()), nil
 }
