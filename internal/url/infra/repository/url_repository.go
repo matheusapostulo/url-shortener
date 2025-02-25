9bdcb5dddde20bc8e7882ef51b80da937dd218c8
matheusapostulo <matheusapostulo10@gmail.com>
2025-02-25 16:18:10 -0300

    feat: Url repository with tests

--- internal/url/infra/repository/url_repository.go
+++ internal/url/infra/repository/url_repository.go
@@ -17,21 +17,69 @@ type URLRepositoryDatabase struct {
 }
 
 func (u *URLRepositoryDatabase) GetNewAvailableID() (int, error) {
-	return 0, nil
+	query := "SELECT MAX(id) FROM urls"
+	row := u.db.QueryRow(query)
+
+	var id int
+	err := row.Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id + 1, nil
 }
 
 func (u *URLRepositoryDatabase) FindByShortURL(shortURL string) (*domain.URL, error) {
-	return &domain.URL{}, nil
+	query := "SELECT id, long_url, short_url FROM urls WHERE short_url = ?"
+	var url domain.URL
+
+	row := u.db.QueryRow(query, shortURL)
+
+	err := row.Scan(&url.ID, &url.LongURL, &url.ShortURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &url, nil
 }
 
 func (u *URLRepositoryDatabase) FindByLongURL(longURL string) (*domain.URL, error) {
-	return &domain.URL{}, nil
+	query := "SELECT id, long_url, short_url FROM urls WHERE long_url = ?"
+	var url domain.URL
+
+	row := u.db.QueryRow(query, longURL)
+
+	err := row.Scan(&url.ID, &url.LongURL, &url.ShortURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &url, nil
 }
 
 func (u *URLRepositoryDatabase) Save(url *domain.URL) error {
+	query := "INSERT INTO urls (long_url, short_url) VALUES (?, ?)"
+	result, err := u.db.Exec(query, (*url).LongURL, (*url).ShortURL)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	(*url).ID = int(id)
+
 	return nil
 }
 
 func (u *URLRepositoryDatabase) Delete(id int) error {
+	query := "DELETE FROM urls WHERE id = ?"
+	_, err := u.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
