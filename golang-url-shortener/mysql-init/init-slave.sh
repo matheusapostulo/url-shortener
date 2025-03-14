681ee2aec29fd6963a8a4d71773da2481a76fce7
matheusapostulo <matheusapostulo10@gmail.com>
2025-03-14 20:33:23 -0300

    refact: Replicating data with mysql GTID

--- golang-url-shortener/mysql-init/init-slave.sh
+++ golang-url-shortener/mysql-init/init-slave.sh
@@ -5,9 +5,6 @@ until mysql -h mysql-master -u root -proot -e "SHOW DATABASES;" > /dev/null 2>&1
     sleep 5
 done
 
-MASTER_LOG_FILE=$(mysql -h mysql-master -u root -proot -e "SHOW BINARY LOG STATUS\G" | grep File | awk '{print $2}')
-MASTER_LOG_POS=$(mysql -h mysql-master -u root -proot -e "SHOW BINARY LOG STATUS\G" | grep Position | awk '{print $2}')
-
-mysql -h localhost -u root -proot -e "STOP REPLICA; CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql-master', SOURCE_USER='root', SOURCE_PASSWORD='root', SOURCE_LOG_FILE='$MASTER_LOG_FILE', SOURCE_LOG_POS=$MASTER_LOG_POS; START REPLICA;"
+mysql -h localhost -u root -proot -e "STOP REPLICA; CHANGE REPLICATION SOURCE TO SOURCE_HOST='mysql-master', SOURCE_USER='root', SOURCE_PASSWORD='root', SOURCE_AUTO_POSITION=1; START REPLICA;"
 
 echo "MySQL Slave is up and running!"
